97557c2794d14d1696292c1de175b22f60abcc1b
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2024-04-09 20:38:28 +0800

    added vitals dart

---
+++ lib/screens/details/vitals.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Vitals extends StatelessWidget {
+  const Vitals({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
