7a6dbf4ba4e3f3e63176de73feeb56ef72994576
Cleverson Sucalit <cleversonsucalit@gmail.com>
2024-04-09 18:56:40 +0800

    patient info stuff

--- lib/screens/patient_info.dart
+++ lib/screens/patient_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PatientInfoPage extends StatefulWidget {
   const PatientInfoPage({super.key});
@@ -17,11 +18,10 @@ class _PatientInfoPageState extends State<PatientInfoPage> {
           child: Center(
             child: Column(
               children: [
-                Container(
-                  height: 150,
-                  width: 400,
-                  color: Colors.amber,
-                ),
+                name(),
+                const SizedBox(height: 20),
+                details(),
+                const SizedBox(height: 20),
 
                 //test
               ],
@@ -30,5 +30,117 @@ class _PatientInfoPageState extends State<PatientInfoPage> {
         ),
       ),
     );
+  }
+
+  Widget name() {
+    return SizedBox(
+      height: 150,
+      width: 400,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            width: 150,
+            height: 150,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/samplePatient.png'),
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Real Niggas',
+                  style: GoogleFonts.inter(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xFFFE8570))),
+              const SizedBox(height: 5),
+              Text('June 26, 2024',
+                  style: GoogleFonts.inter(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                      color: const Color(0xFFFE8570))),
+              Text('Date Admitted',
+                  style: GoogleFonts.inter(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.italic,
+                      color: const Color(0xFFFE8570))),
+            ],
+          ),
+          SizedBox(width: 50)
+        ],
+      ),
+    );
+  }
+
+  Widget details() {
+    return SizedBox(
+      width: 400,
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          age(),
+          weight(),
+          bloodType(),
+          room(),
+        ],
+      ),
+    );
+  }
+
+  Column age() {
+    return Column(
+      children: [
+        detailHead('Age'),
+        detailData('68'),
+      ],
+    );
+  }
+
+  Column weight() {
+    return Column(
+      children: [
+        detailHead('Weight'),
+        detailData('58 Kg'),
+      ],
+    );
+  }
+
+  Column bloodType() {
+    return Column(
+      children: [
+        detailHead('Blood Type'),
+        detailData('O+'),
+      ],
+    );
+  }
+
+  Column room() {
+    return Column(
+      children: [
+        detailHead('Room no.'),
+        detailData('301'),
+      ],
+    );
+  }
+
+  Widget detailData(String content) {
+    return Text(content,
+        style: GoogleFonts.inter(
+            fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black));
+  }
+
+  Widget detailHead(String content) {
+    return Text(content,
+        style: GoogleFonts.inter(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: const Color(0xFFFE8570)));
   }
 }
